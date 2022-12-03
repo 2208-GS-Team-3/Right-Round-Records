3a22c775aaf9116a8b9ac0fbfbec2b81413ee16c
AOKaizoMac <kolbywolf@gmail.com>
2022-12-03 17:58:15 -0500

    Log-in MUI Update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import store from './store';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const container = document.querySelector('#root');
 const root = createRoot(container);
@@ -11,7 +12,9 @@ const root = createRoot(container);
 root.render(
     <Provider store={store}>
         <BrowserRouter>
+        <CssBaseline>
             <App />
+        </CssBaseline>
         </BrowserRouter>
     </Provider>
 );
