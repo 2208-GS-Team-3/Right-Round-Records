75dca79ff85979e4bc4841c9e736a7fa2f5dbe1a
ocjarman <ocjarman@gmail.com>
2022-12-12 13:10:06 -0500

    updated filter component with mui

--- src/components/FilterGenre.js
+++ src/components/FilterGenre.js
@@ -1,44 +1,49 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 const FilterGenre = () => {
   const records = useSelector((state) => state.records.records);
   const genres = useSelector((state) => state.genres.genres);
   const [genreFilter, setGenreFilter] = useState("all");
 
-  //cant quite test this out without the associations being made in the seed btw genres and records
-  //genreFilter should determine genres displaying on UI
-  let recordsToDisplay = records.filter(function (record) {
-    if (genreFilter !== "all") {
-      //   console.log(record.genre);
-      //   return record.genres.includes(genreFilter);
-    } else {
-      return records;
-    }
-  });
+  // let recordsToDisplay = records.filter(function (record) {
+  //   if (genreFilter !== "all") {
+  //     //   console.log(record.genre);
+  //     //   return record.genres.includes(genreFilter);
+  //   } else {
+  //     return records;
+  //   }
+  // });
   //   console.log(recordsToDisplay);
 
-  const changeFilterValue = (e) => {
+  const changeGenre = (e) => {
     e.preventDefault();
     setGenreFilter(e.target.value);
   };
 
   return (
     <>
-      <form>
-        <label>
-          Filter Records by Genre:
-          <select key={"selector"} onChange={changeFilterValue} defaultValue={"all"}>
-            <option key={"all"} value="all">All Genres</option>
-            {genres.map((genre) => (
-              <option value={genre.name} key={`${genre?.name}`}>
-                {genre?.name}
-              </option>
-            ))}
-          </select>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <FormControl style={{ width: "200px" }}>
+        <InputLabel id="demo-simple-select-label">Filter by Genre</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={genreFilter}
+          label="genrefilter"
+          onChange={changeGenre}
+        >
+          {genres.map((genre) => (
+            <MenuItem value={genre} key={genre.id}>
+              {genre.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
     </>
   );
 };
