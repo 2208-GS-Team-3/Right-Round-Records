7c95c9f5d36869c655eb0e3b603fc7d6340135fe
ocjarman <ocjarman@gmail.com>
2022-12-15 13:27:12 -0500

    basic front end validation for credit card info

--- src/components/Checkout/PaymentForm.js
+++ src/components/Checkout/PaymentForm.js
@@ -2,14 +2,46 @@ import * as React from "react";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
-
+import { Form } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { setCreditCard } from "../../store/checkoutSlice";
 import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { FormControl, FormHelperText } from "@mui/material";
 
+const dateFormat = /^\d{1,2}\/\d{2,4}$/;
 const PaymentForm = () => {
   const creditCard = useSelector((state) => state.checkoutData.creditCard);
+  const [validity, setValidity] = useState({
+    creditCardNum: false,
+    expiryDate: false,
+    cvv: false,
+  });
   const dispatch = useDispatch();
+
+  console.log(creditCard);
+  const validateCreditCard = () => {
+    if (creditCard?.creditCardNum?.length === 15) {
+      setValidity({ ...validity, creditCardNum: true });
+    } else {
+      setValidity({ ...validity, creditCardNum: false });
+    }
+  };
+  const validateExpiry = () => {
+    if (creditCard?.expiryDate?.length === 4) {
+      setValidity({ ...validity, expiryDate: true });
+    } else {
+      setValidity({ ...validity, expiryDate: false });
+    }
+  };
+
+  const validateCvv = () => {
+    if (creditCard?.ccSecurity?.length === 2) {
+      setValidity({ ...validity, cvv: true });
+    } else {
+      setValidity({ ...validity, cvv: false });
+    }
+  };
 
   const handleCheckoutStateChange = (e) => {
     const target = e.target;
@@ -18,12 +50,16 @@ const PaymentForm = () => {
     dispatch(setCreditCard({ ...creditCard, [name]: value }));
   };
 
+  if (Object.values(validity).includes(false)) {
+    console.log("dont allow next");
+  }
+
   return (
     <>
       <Typography variant="h6" gutterBottom>
-        Payment methoddd
+        Payment method
       </Typography>
-      <form>
+      <Form className="form">
         <Grid container spacing={3}>
           <Grid item xs={12} md={6}>
             <TextField
@@ -38,44 +74,74 @@ const PaymentForm = () => {
             />
           </Grid>
           <Grid item xs={12} md={6}>
-            <TextField
-              required
-              id="cardNumber"
-              label="Card number"
-              name="creditCardNum"
-              fullWidth
-              autoComplete="cc-number"
-              variant="standard"
-              onChange={handleCheckoutStateChange}
-            />
+            <FormControl error={validity.creditCardNum} required>
+              <TextField
+                required
+                id="cardNumber"
+                label="Card number"
+                name="creditCardNum"
+                fullWidth
+                autoComplete="cc-number"
+                variant="standard"
+                onChange={(event) => {
+                  handleCheckoutStateChange(event);
+                  validateCreditCard(event);
+                }}
+              />
+              <FormHelperText
+                id="username-helper-text"
+                style={{ color: "red" }}
+              >
+                {!validity.creditCardNum && "credit card must be 16 digits."}
+              </FormHelperText>
+            </FormControl>
           </Grid>
           <Grid item xs={12} md={6}>
-            <TextField
-              required
-              id="expDate"
-              label="Expiry date"
-              name="expiryDate"
-              fullWidth
-              autoComplete="cc-exp"
-              variant="standard"
-              onChange={handleCheckoutStateChange}
-            />
+            <FormControl error={validity.expiryDate} required>
+              <TextField
+                required
+                id="expDate"
+                label="Expiry date"
+                name="expiryDate"
+                fullWidth
+                autoComplete="cc-exp"
+                variant="standard"
+                onChange={(event) => {
+                  handleCheckoutStateChange(event);
+                  validateExpiry(event);
+                }}
+              />
+              <FormHelperText
+                id="username-helper-text"
+                style={{ color: "red" }}
+              >
+                {!validity.expiryDate && "enter date with format (mm/yy)"}
+              </FormHelperText>
+            </FormControl>
           </Grid>
           <Grid item xs={12} md={6}>
-            <TextField
-              required
-              id="cvv"
-              label="CVV"
-              name="ccSecurity"
-              helperText="Last three digits on signature strip"
-              fullWidth
-              autoComplete="cc-csc"
-              variant="standard"
-              onChange={handleCheckoutStateChange}
-            />
+            <FormControl error={validity.expiryDate} required>
+              <TextField
+                required
+                id="cvv"
+                label="CVV"
+                name="ccSecurity"
+                helperText="Last three digits on signature strip"
+                fullWidth
+                autoComplete="cc-csc"
+                variant="standard"
+                onChange={(event) => {
+                  handleCheckoutStateChange(event);
+                  validateCvv(event);
+                }}
+              />
+              <FormHelperText id="cvv-helper-text" style={{ color: "red" }}>
+                {!validity.cvv && "enter valid cvv"}
+              </FormHelperText>
+            </FormControl>
           </Grid>
         </Grid>
-      </form>
+      </Form>
     </>
   );
 };
