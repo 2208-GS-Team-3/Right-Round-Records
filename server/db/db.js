452636609ff999227af3075d34ff5f9578589b9e
AOKaizoMac <kolbywolf@gmail.com>
2022-12-15 20:44:36 -0500

    Added internal postgres url

--- server/db/db.js
+++ server/db/db.js
@@ -5,6 +5,6 @@ const config = {
 
 const URL = `postgres://rightroundrecords_user:010PKc0P3HaZVNN97RN3mjHZQUEDKH09@dpg-cedsmehgp3jvikbpuo90-a/rightroundrecords`;
 
-const db = new Sequelize(process.env.DATABASE_URL || URL, config);
+const db = new Sequelize( URL, config || process.env.DATABASE_URL);
 
 module.exports = db;
